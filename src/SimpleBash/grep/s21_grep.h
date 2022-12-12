cf05206621b205726e77790034c04fa2a5c816af
Mammie Mikken <mammiemi@ga-b3.msk.21-school.ru>
2022-12-12 22:23:02 +0300

    style ok

--- src/SimpleBash/grep/s21_grep.h
+++ src/SimpleBash/grep/s21_grep.h
@@ -1,9 +1,11 @@
 #ifndef SRC_GREP_S21_GREP_H_
 #define SRC_GREP_S21_GREP_H_
+#include <getopt.h>
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define buff_size 999999
 
 struct opt_grep {
   int opt_e;
@@ -12,11 +14,16 @@ struct opt_grep {
   int opt_c;
   int opt_l;
   int opt_n;
-  int file_n;
+  int opt_h;
+  int opt_s;
+  int opt_f;
+  int opt_o;
 };
 
-int parser(int count, char **pars_str, struct opt_grep *pars, char *template);
-int file_open(int argc, char **pars_str, struct opt_grep *pars, char *template);
-void print_file(char **str, int x, struct opt_grep *pars, char *template);
+int parser(int argc, char *argv[], struct opt_grep *pars, char *template);
+void filework(struct opt_grep *pars, FILE *fp, regex_t reg, char *file);
+void grep_file(struct opt_grep *pars, char *template, char *file);
+int save_patt_file(char *template, char *temp);
+void grep(struct opt_grep *pars, int argc, char **argv, char *template);
 
 #endif  // SRC_GREP_S21_GREP_H_
